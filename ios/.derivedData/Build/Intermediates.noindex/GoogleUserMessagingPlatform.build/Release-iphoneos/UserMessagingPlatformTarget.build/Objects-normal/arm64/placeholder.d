2952e3775758e64b3fb77dbe02e113d47414c2ad
Yusuf Ali <y666suf@Yusufs-MacBook.local>
2026-05-25 11:20:06 +0100

    checkpoint before checking out emergent/ui-polish-2026

---
+++ ios/.derivedData/Build/Intermediates.noindex/GoogleUserMessagingPlatform.build/Release-iphoneos/UserMessagingPlatformTarget.build/Objects-normal/arm64/placeholder.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/y666suf/Documents/nfg-crash/ios/.derivedData/SourcePackages/checkouts/swift-package-manager-google-user-messaging-platform/UserMessagingPlatformTarget/placeholder.m
---
+++ ios/DerivedData/Build/Intermediates.noindex/ArchiveIntermediates/NFGCrash/IntermediateBuildFilesPath/GoogleUserMessagingPlatform.build/Release-iphoneos/UserMessagingPlatformTarget.build/Objects-normal/arm64/placeholder.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/y666suf/Documents/nfg-crash/ios/DerivedData/SourcePackages/checkouts/swift-package-manager-google-user-messaging-platform/UserMessagingPlatformTarget/placeholder.m
